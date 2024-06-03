ddea564973d81a53bc46bd08b32a16de79cd0410
Buvaneshwari-143 <buvannna@gmail.com>
2024-06-03 16:28:37 +0530

    my assignments

---
+++ day1/FireFox.java
@@ -0,0 +1,24 @@
+package week1.day1;
+
+public class FireFox {
+	
+public static void main(String[]args) {
+		
+		float browserVersion = 100.2f;
+		String browserName = "FireFox";
+		boolean isVisible = true;
+		int releaseYear = 1998;
+		char browserLogo = 'f' ;
+		
+		System.out.println("browser Version is "+browserVersion);
+		System.out.println("Name :" +browserName );
+		System.out.println("Visible:" + isVisible ); 
+		System.out.println("ReleaseYear:" +releaseYear );
+		System.out.println("LOGO:" +browserLogo );
+		
+		}
+
+}
+
+
+
